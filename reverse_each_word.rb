b662b611827bab5a6c9ce4c28178b2287cc042d7
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-19 16:27:16 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string)
+  split_string = string.split(' ')
+  split_string.each do |x|
+    x.reverse!
+  end
+  return split_string.join(" ")
+end
+
+def reverse_each_word(string)
+  
+end
