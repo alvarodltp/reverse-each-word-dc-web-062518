d31fa377cc930a1d3c002d535db201c4b7f606ea
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-19 16:28:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,9 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  
+  split_string = string.split(" ")
+  split_string.collect do |x|
+    x.reverse!
+  end
+  return split_string.join(" ")
 end
